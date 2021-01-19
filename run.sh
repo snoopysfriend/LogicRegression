55507cbfda13ec061dca62cf8a19c3de943bbe93
snoopysfriend <b05902045@ntu.edu.tw>
2021-01-19 18:17:03 +0800

    Modfied script

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./lrg ./data/final_cases_publish_1009/case4/io_info.txt ./data/final_cases_publish_1009/case4/iogen circuit.v
+./lrg ./data/final_cases_publish_1009/case4/io_info.txt ./data/final_cases_publish_1009/case4/iogen circuit.v 1
--- src/run.sh
+++
@@ -1,1 +0,0 @@
-./lrg ../data/final_cases_publish_1009/case4/io_info.txt ./../data/final_cases_publish_1009/case4/iogen circuit.v
